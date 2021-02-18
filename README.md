31c95641b05b6f255efc81688eac6a278cf6830e
cijhodges <jhodges@us.ci.org>
2021-02-18 17:20:00 -0600

    0.0.2 - Add README and views
    - Added a simple README.md
    - Included redirect login and page views instead of code inside Main class

---
+++ README.md
@@ -0,0 +1,18 @@
+# Okta Integration Options
+
+Force users to authenticate through Okta Single Sign-On for the entirety or parts of your website.
+
+## Where to Find the Options Page
+
+Navigate to the options page under *Tools > Okta Integration*.
+
+## Options
+
+- **Force Authentication**: This option allows you to force all users to be authenticated to view your entire website.
+- **SAML Login URL**: If you'd like to specify a specific login URL, you can do so with this field.
+- **Referral Domains**:  If the referring domain is found, then the user will be navigated to the last page the user was on before they were redirected to be authenticated.
+- **Pages Forced to Authenticate**: You may declare specific pages off limits to unauthenticated users if the *Force Authentication* option is not selected.
+
+## Page / Post Options
+
+At the bottom of each page and post, you will find a metabox named *Okta Integration Options*. Choose *Force Okta Authentication* to force the user to be authenticated to view the page or post.
--- models/Main.php
+++ models/Main.php
@@ -219,34 +219,7 @@ class Main
             ) {
                 $login_page = '/wp-login.php';
                 if ( $this->saml_login ) $login_page = $this->saml_login;
-?>
-                <script type="text/javascript">
-                    (function() {
-                        setCookie('oktaIntegrationLastPage', window.location.href, 1);
-                        window.location.href = '<?= $login_page; ?>';
-
-                        function setCookie(name,value,days) {
-                            var expires = "";
-                            if (days) {
-                                var date = new Date();
-                                date.setTime(date.getTime() + (days*24*60*60*1000));
-                                expires = "; expires=" + date.toUTCString();
-                            }
-                            document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-                        }
-                        function getCookie(name) {
-                            var nameEQ = name + "=";
-                            var ca = document.cookie.split(';');
-                            for(var i=0;i < ca.length;i++) {
-                                var c = ca[i];
-                                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                            }
-                            return null;
-                        }
-                    })();
-                </script>
-<?php
+                include_once OKTA_INTEGRATION_PATH . '/views/redirect-login.php';
                 die();
             }
 
@@ -259,36 +232,7 @@ class Main
             && isset( $_COOKIE['oktaIntegrationLastPage'] ) 
             && $page !== $_COOKIE['oktaIntegrationLastPage']
         ) {
-?>
-                <script type="text/javascript">
-                    (function() {
-                        var lastPage = getCookie('oktaIntegrationLastPage');
-
-                        setCookie('oktaIntegrationLastPage', null, -1);
-                        window.location.href = lastPage;
-
-                        function setCookie(name,value,days) {
-                            var expires = "";
-                            if (days) {
-                                var date = new Date();
-                                date.setTime(date.getTime() + (days*24*60*60*1000));
-                                expires = "; expires=" + date.toUTCString();
-                            }
-                            document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-                        }
-                        function getCookie(name) {
-                            var nameEQ = name + "=";
-                            var ca = document.cookie.split(';');
-                            for(var i=0;i < ca.length;i++) {
-                                var c = ca[i];
-                                while (c.charAt(0)==' ') c = c.substring(1,c.length);
-                                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-                            }
-                            return null;
-                        }
-                    })();
-                </script>
-<?php
+            include_once OKTA_INTEGRATION_PATH . '/views/redirect-page.php';
             die();
         }
 	}
